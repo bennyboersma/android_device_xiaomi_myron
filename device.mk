22c5de3d0b2ca79ae0ebc1b0d0028325a54efa06
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-08-22 12:45:09 +0200

    myron: Import AOSP audio_policy_engine configs
    Change-Id: Icf55b3753c1a28cf7231685b437d17b0a52d1b92

--- device.mk
+++ device.mk
@@ -11,6 +11,13 @@ $(call inherit-product, device/xiaomi/sm8850-common/common.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/myron/myron-vendor.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_policy_engine_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy_engine_default_stream_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_default_stream_volumes.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy_engine_product_strategies.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_product_strategies.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy_engine_stream_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_stream_volumes.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator-V1-ndk.vendor \
